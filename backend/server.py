d5e8dc9dfbe5972f2ec198347f73e5f3f004e645
Kartik Soneji <kartiksoneji@rocketmail.com>
2021-07-25 19:54:20 +0530

    Server static frontend from Flask.

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,24 @@
 import flask
 import base64
+from ics import Calendar
 
 from main import get_hackathon_calendars
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='')
 
+@app.route('/')
+def send_index():
+    return flask.send_from_directory("../frontend", "index.html")
 
 @app.route("/events")
 def get_events():
     hackathon_calendars = get_hackathon_calendars("https://mlh.io/seasons/2022/events/")
+    
+    all_hackathon_calendars = Calendar()
+    for hackathon in hackathon_calendars:
+        all_hackathon_calendars.events.update(hackathon_calendars[hackathon].events)
+    hackathon_calendars["ALL_CALENDARS"] = all_hackathon_calendars
+    
     for hackathon in hackathon_calendars:
         hackathon_calendars[hackathon] = base64.b64encode(
             str(hackathon_calendars[hackathon]).encode()
